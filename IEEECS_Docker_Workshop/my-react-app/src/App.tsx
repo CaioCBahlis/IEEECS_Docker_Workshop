173acf06265f910d268593ddc6f47879a7f0605a
Caio Carraro Bahlis <caiobahlis@eduroam-226-230-182.laptops.usf.edu>
2026-02-26 17:53:17 -0500

    Changed layout

--- IEEECS_Docker_Workshop/my-react-app/src/App.tsx
+++ IEEECS_Docker_Workshop/my-react-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> Docker + IEEE-CS + Go </h1>
+      <h1> Docker - It works on my machine </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
